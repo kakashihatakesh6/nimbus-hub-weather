9f027fff090d87d44e6bdb7e6f29a5f75dfaedf9
LordFifthMinato <imnikhil143@gmail.com>
2022-02-06 22:19:38 +0530

    first commit of prg file in heroku repo

---
+++ prg.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DataMe</title>
+</head>
+<body>
+    <header>
+       <div>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Login</a>
+            <a href="">Register</a>
+            
+        </nav>
+       </div>
+    </header>
+</body>
+</html>
